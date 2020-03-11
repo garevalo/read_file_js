d71702811c125f0c942778731e4afc42dd1a63db
Giordan Arevalo <gbap0506@hotmail.com>
2020-03-10 23:52:15 -0500

    commit

--- find.js
+++ find.js
@@ -14,44 +14,49 @@ const openFile = (event) => {
 	  let text_to_search = document.getElementById("text_to_search").value
 	  let eachLine = text.split('\n')
 	  let output = ""
-	  let count = 0
+	  let countNormal = 0
+	  let countUpper = 0
+	  let countUcfirst = 0
 	  
-	  //toUpperCase
+	  let upper   = text_to_search.trim().toUpperCase()
+  	  let ucFirst = firstWordToUpper(text_to_search.trim())
+  	  let normal 	= text_to_search.trim()	
 
 	  for( let i = 0; i <  eachLine.length; i++ ) {
 	     
-	  	  let upper   = text_to_search.trim().toUpperCase()
-      	  let ucFirst = firstWordToUpper(text_to_search.trim())
-      	  let normal 	= text_to_search.trim()	
-
 	      if( 
-	      	eachLine[i].search( normal ) != -1 ||
-	      	eachLine[i].search( upper ) != -1 ||
-	      	eachLine[i].search( ucFirst )  != -1
+	      	eachLine[i].search( normal ) 	!= -1 ||
+	      	eachLine[i].search( upper ) 	!= -1 ||
+	      	eachLine[i].search( ucFirst )  	!= -1
 	      	){
-
-	      
 
 	      	if(  eachLine[i].search( normal ) != -1 ){
 	      		output += render_output(i,eachLine[i],normal);
+	      		countNormal++;
 	      	}
 	      	
 	      	if( eachLine[i].search( upper ) != -1 ){
 	      		output += render_output(i,eachLine[i],upper);
+	      		countUpper++;
 	      	}
 
 	      	if( eachLine[i].search( ucFirst )  != -1 ){
 	      		output += render_output(i,eachLine[i],ucFirst);
+	      		countUcfirst++;
 	      	}
 			
-	      	count++;
-	     
 	      }
 	  }	
 
-	  node.innerHTML = `${output} <div class="row">  
-	  								<div class="mt-3 alert alert-primary"> TOTAL :  <span class="font-weight-bold text-primary"> ${count} </span> </div> 
-	  							  </div>`;
+	  node.innerHTML = `${output} 
+	  					<hr>
+	  					<div class="row">  
+							<div class="mt-3 alert alert-primary"> Total <span class="font-weight-bold ">${normal}</span> :  <span class="font-weight-bold "> ${countNormal} </span> </div> 
+						 
+							<div class="mt-3 ml-2 alert alert-danger"> Total <span class="font-weight-bold ">${upper}</span> :  <span class="font-weight-bold "> ${countUpper} </span> </div> 
+						
+							<div class="mt-3 ml-2 alert alert-warning"> Total <span class="font-weight-bold ">${ucFirst}</span> :  <span class="font-weight-bold "> ${countUcfirst} </span> </div> 
+						</div>`;
 
 	};
 
@@ -99,6 +104,6 @@ const render_output = (number,string,text) => {
 
 }
 
-function firstWordToUpper(string){
-  return string.charAt(0).toUpperCase() + string.slice(1);
-}
+
+
+const firstWordToUpper = (string) => string.charAt(0).toUpperCase() + string.slice(1)
